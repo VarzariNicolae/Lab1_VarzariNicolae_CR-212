e1a0feeee922241ec1e85c4563fcaeac05d55a75
TwebVarzariNicolae <124369614+TwebVarzariNicolae@users.noreply.github.com>
2023-02-24 21:28:11 +0200

    Modificari laborator 5

--- my-react-project/my-app/src/App.tsx
+++ my-react-project/my-app/src/App.tsx
@@ -1,65 +1,62 @@
-import React from 'react';
-import './lab4.css';
+import React, { useState } from 'react';
+import { useStore } from './MyStore';
+import './index.css';
 
-interface User {
-    name: string;
-    email: string;
-    age: number;
-    address: string;
-    isActive: boolean;
-}
+function MyComponent() {
+    const store = useStore();
 
-interface AdvancedUser extends User {
-    education: string;
-    skills: string[];
-}
+    // local state
+    const [localData, setLocalData] = useState<any>({ name: '', address: '', phone: '' });
 
-const user: AdvancedUser = {
-    name: 'Varzari Nicolae',
-    email: 'varzari@gmail.com',
-    age: 21,
-    address: 'Vasile Alecsandri 12',
-    isActive: true,
-    education: 'Administrator Aplicații WEB',
-    skills: ['JavaScript', 'React', 'Node.js'],
-};
+    // use data from MobX store
+    const myData = store.myData;
 
-function Lab4() {
+    // update data in MobX store
+    // add new object to MobX store
+    const handleAddObject = (event: React.FormEvent<HTMLFormElement>) => {
+
+        event.preventDefault();
+        if (!localData.name || !localData.address || !localData.phone) {
+            alert('Introduceti toate campurile.');
+            return;
+        }
+        store.addObject(localData);
+        setLocalData({ name: '', address: '', phone: '' });
+    };
+
+    // delete object from MobX store
+// delete object from MobX store and update local state
+    const handleDeleteObject = (index: number) => {
+        store.deleteObject(index);
+        setLocalData({ name: "", address: "", phone: "" });
+    };
+
+    // update object in MobX store
+
     return (
+
         <div className="container">
-            <h1 className="title">Date Personale</h1>
-            <div className="card">
-                <p>
-                    <span className="label">Name:</span>
-                    <span className="value">{user.name}</span>
-                </p>
-                <p>
-                    <span className="label">Email:</span>
-                    <span className="value">{user.email}</span>
-                </p>
-                <p>
-                    <span className="label">Age:</span>
-                    <span className="value">{user.age}</span>
-                </p>
-                <p>
-                    <span className="label">Address:</span>
-                    <span className="value">{user.address}</span>
-                </p>
-                <p>
-                    <span className="label">Active:</span>
-                    <span className="value">{user.isActive.toString()}</span>
-                </p>
-                <p>
-                    <span className="label">Education:</span>
-                    <span className="value">{user.education}</span>
-                </p>
-                <p>
-                    <span className="label">Skills:</span>
-                    <span className="value">{user.skills.join(', ')}</span>
-                </p>
+            <h1 className="title">Varzari Nicolae Laboratorul 5</h1>
+            <form className="form" onSubmit={handleAddObject}>
+                <input className="input" type="text" placeholder="Nume" value={localData.name} onChange={e => setLocalData({ ...localData, name: e.target.value })} />
+                <input className="input" type="text" placeholder="Adresa" value={localData.address} onChange={e => setLocalData({ ...localData, address: e.target.value })} />
+                <input className="input" type="text" placeholder="Telefon" value={localData.phone} onChange={e => setLocalData({ ...localData, phone: e.target.value })} />
+                <button className="button" type="submit">Trimite</button>
+            </form>
+            <div>
+                <div className="grid">
+                    {myData.map((data: any, index: number) => (
+                        <div key={index} className="object">
+                            <p>Nume: {data.name}</p>
+                            <p>Adresa: {data.address}</p>
+                            <p>Telefon: {data.phone}</p>
+                            <button onClick={() => handleDeleteObject(index)}>Delete</button>
+                        </div>
+                    ))}
+                </div>
             </div>
         </div>
     );
 }
 
-export default Lab4;
+export default MyComponent;
